4dc120e33f1ca4e22c55afc87551e57aa7e257ba
Raflizar Deswandi Yahya <raflizardeswandiy24@gmail.com>
2023-02-13 16:10:49 +0700

    fix bug

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -29,7 +29,6 @@ class Food extends Model
         $query->when($filters['search'] ?? null, function ($query, $search) {
             $query->where(function ($query) use ($search) {
                 $query->where('name', 'like', '%' . $search . '%')
-                    ->orWhere('description', 'like', '%' . $search . '%')
                     ->orWhereHas('category', function ($query) use ($search) {
                         $query->where('name', 'like', '%' . $search . '%');
                     });
